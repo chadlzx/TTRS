e25e813398e4290a95f082c36a44c46a4559a4e0
yujie6 <yujie6@sjtu.edu.cn>
2019-05-13 17:31:21 +0800

    Erase: too many cases..

--- BplusTree/test.cpp
+++ BplusTree/test.cpp
@@ -7,9 +7,19 @@ using std::cout;
 
 int main(){
 	sjtu::Bptree<int, int> a;
-	for (int i = 5; i <= 17; i++) a.insert( std::pair<int, int>(i, 2 * i));
+	/*for (int i = 5; i <= 19; i++) a.insert( std::pair<int, int>(i, 2 * i));
+	a.Print();
 	cout << a.size() << "\n";
-	cout << a.at(5) << " ";
-	cout << a.at(16) << " ";
+	a.erase(19);
+	a.erase(18);
+	a.Print();*/
+	//cout << a.at(16) << " ";
+
+	for (int i = 5; i <= 19; i++) a.insert( std::pair<int, int>(i, 2 *i));
+	a.Print();
+	for (int i = 5; i <= 10; i++) {
+	    a.erase(i); //checking i = 7 now
+	    a.Print();
+	}
 	return 0;
 }
