e58705fdc0dbcd0ef9482f32d8f0f4721ed07bd5
yujie6 <yujie6@sjtu.edu.cn>
2019-05-14 23:49:39 +0800

    rebuild start

--- BplusTree/NewBptree.hpp
+++ BplusTree/NewBptree.hpp
@@ -1,1 +1,36 @@
-placeholder
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <cassert>
+#include "./utility.hpp"
+#include "./exceptions.hpp"
+
+#ifndef BPTREE_HPP
+#define BPTREE_HPP
+
+namespace sjtu {
+    /*
+     *  Attention : In this file I use std::pair & std::map
+     *     replace them with sjtu:pair when finishing development.
+     */
+    template<
+            class Key,
+            class T,
+            class Compare = std::less<Key>,
+            int IndexSize = 5,
+            int PageSize = 5
+    >
+    class bptree{
+    public:
+
+
+    private:
+
+    };
+
+
+}
+
+#endif
--- BplusTree/test.cpp
+++ BplusTree/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "Bptree.hpp"
+#include "NewBptree.hpp"
 #include "utility.hpp"
+#include "paragon.hpp"
 using std::cout;
 
 
@@ -15,10 +16,10 @@ int main(){
 	 *  TODO: last time, I try to figure out when erase 5, there is a problem
 	 *   when merging(forget root perhaps), later I found that data is not correct,
 	 */
-	for (int i = 5; i <= 30; i++) {
+	/*for (int i = 5; i <= 30; i++) {
 	    a.insert( std::pair<int, int>(i, 2 *i));
 	}
-	a.Print();
+	a.Print();*/
 	/*for (int i = 5; i <= 15; i++) {
 	    a.erase(i); //checking i = 5 now
 	    a.Print();
