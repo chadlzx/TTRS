e9c3507a152d337008b5f99ea81174473bb56ab6
yujie6 <yujie6@sjtu.edu.cn>
2019-05-14 23:44:39 +0800

    rebuild start

--- BplusTree/test.cpp
+++ BplusTree/test.cpp
@@ -7,19 +7,24 @@ using std::cout;
 
 int main(){
 	sjtu::Bptree<int, int> a;
-	/*for (int i = 5; i <= 19; i++) a.insert( std::pair<int, int>(i, 2 * i));
-	a.Print();
-	cout << a.size() << "\n";
-	a.erase(19);
-	a.erase(18);
-	a.Print();*/
-	//cout << a.at(16) << " ";
-
-	for (int i = 5; i <= 19; i++) a.insert( std::pair<int, int>(i, 2 *i));
-	a.Print();
-	for (int i = 5; i <= 10; i++) {
-	    a.erase(i); //checking i = 7 now
-	    a.Print();
+	/*
+	 *  Insert test complete
+	 *  Not correct... maintain data failed
+	 *  Erase test complete 70% I guess
+	 *
+	 *  TODO: last time, I try to figure out when erase 5, there is a problem
+	 *   when merging(forget root perhaps), later I found that data is not correct,
+	 */
+	for (int i = 5; i <= 30; i++) {
+	    a.insert( std::pair<int, int>(i, 2 *i));
 	}
+	a.Print();
+	/*for (int i = 5; i <= 15; i++) {
+	    a.erase(i); //checking i = 5 now
+	    a.Print();
+	}*/
+
+
+
 	return 0;
 }
